d7ff08cf08a96487ed217b9f0c07d2283d8a15b2
Francisco <fsalazarg@ing.ucsc.cl>
2022-02-15 08:02:36 -0300

    musha cosa

--- js/iot-mqtt.js
+++ js/iot-mqtt.js
@@ -1,39 +1,70 @@
-// Declaración de variables
+// Declaración del id del cliente
 var clientId = "ws" + Math.random(); //Dirección aleatoria
-Temperatura = 0;
 
-// Create a client instance
+// Declaración de las variables
+Temperatura1 = 0;
+Temperatura2 = 0;
+Temperatura3 = 0;
+Temperatura4 = 0;
+Temperatura5 = 0;
+Temperatura6 = 0;
+
+// Creación de la instancia del cliente
 var client = new Paho.MQTT.Client("190.110.108.59", 8083, clientId);
 
-// Set callback handlers
+// Se establece el control de Callbacks
 client.onConnectionLost = onConnectionLost;
 client.onMessageArrived = onMessageArrived;
 
-// Connect the client
+// Conexión con el cliente
 client.connect({ onSuccess: onConnect });
 
-// Called when the client connects
+// Función que se llama cuando el cliente se conecta (y se suscribe a los tópicos)
 function onConnect() {
   console.log("Conectado MQTT-WebSocket");
-  client.subscribe("Temp");
+  client.subscribe("Datco/Chile/Oficina_1/Temperatura");
+  client.subscribe("Datco/Chile/Oficina_2/Temperatura");
+  client.subscribe("Datco/Chile/Oficina_3/Temperatura");
+  client.subscribe("Datco/Argentina/Oficina_1/Temperatura");
+  client.subscribe("Datco/Argentina/Oficina_2/Temperatura");
+  client.subscribe("Datco/Argentina/Oficina_3/Temperatura");
   client.subscribe("Relay");
 }
 
-// Called when the client loses its connection
+// Función que se llama cuando el cliente pierde la conexión
 function onConnectionLost(responseObject) {
   if (responseObject.errorCode !== 0) {
-    console.log("Conexión Perdida. Error:" + responseObject.errorMessage);
+    console.log("Conexión con el broker perdida. Error:" + responseObject.errorMessage);
   }
 }
 
-// Called when a message arrives
+// Función que se llama cuando llega un mensaje
 function onMessageArrived(message) {
   console.log(message.destinationName + ": " + message.payloadString);
-  if (message.destinationName == 'Temp') {
-    document.getElementById("ValorTemp").textContent = message.payloadString;
-    Temperatura = parseFloat(message.payloadString);
+  if (message.destinationName == 'Datco/Chile/Oficina_1/Temperatura') {
+    document.getElementById("CH_OF1_T").textContent = message.payloadString;
+    Temperatura1 = parseFloat(message.payloadString);
   }
-
+  if (message.destinationName == 'Datco/Chile/Oficina_2/Temperatura') {
+    document.getElementById("CH_OF2_T").textContent = message.payloadString;
+    Temperatura2 = parseFloat(message.payloadString);
+  }
+  if (message.destinationName == 'Datco/Chile/Oficina_3/Temperatura') {
+    document.getElementById("CH_OF3_T").textContent = message.payloadString;
+    Temperatura3 = parseFloat(message.payloadString);
+  }
+  if (message.destinationName == 'Datco/Argentina/Oficina_1/Temperatura') {
+    document.getElementById("ARG_OF1_T").textContent = message.payloadString;
+    Temperatura4 = parseFloat(message.payloadString);
+  }
+  if (message.destinationName == 'Datco/Argentina/Oficina_2/Temperatura') {
+    document.getElementById("ARG_OF2_T").textContent = message.payloadString;
+    Temperatura5 = parseFloat(message.payloadString);
+  }
+  if (message.destinationName == 'Datco/Argentina/Oficina_3/Temperatura') {
+    document.getElementById("ARG_OF3_T").textContent = message.payloadString;
+    Temperatura6 = parseFloat(message.payloadString);
+  }
   if (message.destinationName == 'Relay') {
     document.getElementById("ValorRelay").textContent = message.payloadString;
   }
